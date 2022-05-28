beec07d2657e479f2a4920bdb60e44884721a248
Erdle, Tobias <tobi.erdle@gmail.com>
2022-05-28 22:20:33 +0200

    Initial commit

---
+++ liberty-mvc/src/main/java/de/erdlet/demo/mvc/MvcApplication.java
@@ -0,0 +1,9 @@
+package de.erdlet.demo.mvc;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/mvc")
+public class MvcApplication extends Application {
+
+}
